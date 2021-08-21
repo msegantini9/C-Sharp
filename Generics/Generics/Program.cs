859a820d857ea97ba1434316e2ba1b68ce876305
Matheus Segantini de Almeida <m.segantini9@gmail.com>
2021-08-21 15:01:53 -0300

    Primeiros teste da aula de Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            List<string> list = new List<string>();
+
+            list.Add("Matheus");
+            list.Add("Bruna");
+            list.Add("Marcos");
+            list.Add("Pedro");
+
+            foreach(string s in list)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+            */
+        }
+    }
+}
