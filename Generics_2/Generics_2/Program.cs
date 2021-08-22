5475b9b4b9b3e56734e6ba63dcfdaba6f28b98a7
Matheus Segantini de Almeida <m.segantini9@gmail.com>
2021-08-21 21:53:31 -0300

    Aula 02 - Generics (IComparable)

---
+++ Generics_2/Generics_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generics_2.Services;
+using Generics_2.Entities;
+using System.Globalization;
+
+namespace Generics_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            CalculationService calculationService = new CalculationService();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(',');
+                string name = s[0];
+                double price = double.Parse(s[1], CultureInfo.InvariantCulture);
+
+                list.Add(new Product(name, price));
+            }
+
+            Console.Write("Max: ");
+            Console.WriteLine(calculationService.max(list));
+        }
+    }
+}
---
+++ Generics_2/Generics_2/Services/CalculationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_2.Services
+{
+    class CalculationService
+    {
+        public T max<T>(List<T> list) where T : IComparable
+        {
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("The list can't be empty!");
+            }
+
+            T max = list[0];
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                if(list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
